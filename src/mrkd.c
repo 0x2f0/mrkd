ff0e5fd751ab17759f7bc8560a677ef2f0f4de0d
0x2f0 <git@xn--81b3bxa1f.com>
2026-06-29 22:57:41 +0545

    feat: added command line parsing

--- src/mrkd.c
+++ src/mrkd.c
@@ -1,17 +1,24 @@
 #include "mrkd.h"
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 int main(int arg_len, char *argv[]) {
-  Config config = parse_args(arg_len, argv);
+  config config = parse_args(arg_len, argv);
+  printf("Parsed Args\n .exit: %d\n .source = %s\n .source_type = %d\n "
+         ".verbose = %d\n .dir_depth = %d\n .output_format = %d\n",
+         config.exit, config.source, config.source_type, config.verbose,
+         config.dir_depth, config.output_format);
+
   if (config.exit)
     return 0;
 
   return 0;
 }
 
-Config parse_args(int arg_length, char *argv[]) {
-  Config config = {.source = NULL,
-                   .source_type = NULL,
+config parse_args(int arg_length, char *argv[]) {
+  config config = {.source = NULL,
+                   .source_type = STR,
                    .verbose = 0,
                    .dir_depth = 1,
                    .output_format = HTML,
@@ -23,21 +30,81 @@ Config parse_args(int arg_length, char *argv[]) {
     return config;
   }
 
+  for (int i = 1; i < arg_length; i++) {
+    // looking for help
+    char *chunk = argv[i];
+
+    if (strcmp(chunk, "-h") == 0 || strcmp(chunk, "--help") == 0) {
+      print_help(argv[0]);
+      config.exit = 1;
+      return config;
+    }
+
+    if (strcmp(chunk, "-s") == 0 || strcmp(chunk, "--source") == 0) {
+      char *path = argv[i + 1];
+      i++;
+      config.source = path;
+      // TODO: check the source type
+      continue;
+    }
+
+    if (strcmp(chunk, "-d") == 0 || strcmp(chunk, "--dir-depth") == 0) {
+      int depth = atoi(argv[i + 1]);
+      i++;
+      config.dir_depth = depth;
+      continue;
+    }
+
+    if (strcmp(chunk, "-v") == 0 || strcmp(chunk, "--verbose") == 0) {
+      config.verbose = 1;
+      continue;
+    }
+
+    if (strcmp(chunk, "-o") == 0 || strcmp(chunk, "--output-format") == 0) {
+      char *output_format = argv[i + 1];
+
+      if (strcmp(output_format, "HTML") == 0) {
+        config.output_format = HTML;
+        i++;
+        continue;
+      }
+
+      if (strcmp(output_format, "AST") == 0) {
+        config.output_format = AST;
+        i++;
+        continue;
+      }
+
+      printf("[%s] is not a valid output format.\nOnly 'HTML' and 'AST' "
+             "output format are supported.\n\n",
+             output_format);
+      config.exit = 1;
+      print_help(argv[0]);
+      return config;
+    }
+
+    printf("invalid option: [%s] \n", chunk);
+    config.exit = 1;
+    print_help(argv[0]);
+    return config;
+  }
+
   return config;
 }
 
 void print_help(char *program_name) {
   int gap = 30;
 
-  printf("Usage: %s [OPTIONS]\n", program_name);
+  printf("Usage: %s [OPTIONS] [VALUE]\n\n", program_name);
   printf("Options: \n");
   printf("%*s %s\n", -gap, "-h, --help ", "Show this help message");
-  printf("%*s %s\n", -gap, "-s, --source=<path>",
-         "string, file_path, dir_path to parse");
-  printf("%*s %s\n", -gap, "-d, --dir_depth=<depth>",
-         "depth of dir to search upto when source is "
+  printf("%*s %s\n", -gap, "-s, --source <path>",
+         "String, file_path, dir_path to parse");
+  printf("%*s %s\n", -gap, "-v, --verbose", "Prints every step in verbosly");
+  printf("%*s %s\n", -gap, "-d, --dir-depth <depth>",
+         "Depth of dir to search upto when source is "
          "dir_path defaults to 1");
-  printf("%*s %s\n", -gap, "-o, --output_format=<format>",
-         "sparsed ouptut format either 'AST' or "
+  printf("%*s %s\n", -gap, "-o, --output-format <format>",
+         "Specify output format either 'AST' or "
          "'HTML' defaults to 'HTML'");
 }
--- src/mrkd.h
+++ src/mrkd.h
@@ -2,23 +2,23 @@
 #define MRKD_H
 
 enum SOURCE_TYPE { STR, FILE_PATH, DIR_PATH };
-enum OUTPUT_FORMAT { AST, HTML };
+enum OUTPUT_FORMAT { HTML, AST };
 
 typedef struct {
   // can be a file, folder or just raw string
   char *source;
-  enum SOURCE_TYPE *source_type;
+  enum SOURCE_TYPE source_type;
   int verbose;
   // depth of dir to search for when source_type is DIR_PATH defaults to 1
   int dir_depth;
   // default to AST
   enum OUTPUT_FORMAT output_format;
-	// to exit the program.
-	int exit;
-} Config;
 
-Config parse_args(int arg_length, char *argv[]);
-/* TODO:implement of feature */
+  // to exit the program.
+  int exit;
+} config;
+
+config parse_args(int arg_length, char *argv[]);
 void print_help(char *program_name);
 
 #endif
