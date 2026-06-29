c593917ff87688445beaeabb1217006f57b31abd
0x2f0 <git@xn--81b3bxa1f.com>
2026-06-29 12:42:34 +0545

    fix: corrected the gap between help command and description

--- src/mrkd.c
+++ src/mrkd.c
@@ -27,14 +27,17 @@ Config parse_args(int arg_length, char *argv[]) {
 }
 
 void print_help(char *program_name) {
+  int gap = 30;
+
   printf("Usage: %s [OPTIONS]\n", program_name);
   printf("Options: \n");
-  printf("-h, --help                      Show this help message\n");
-  printf("-s, --source <path>             string, file_path, "
-         "dir_path to parse\n");
-  printf("-d, --dir_depth <depth>         depth of dir to search upto "
-         "when source is dir_path defaults to 1\n");
-  printf("-o, --output_format <format>    parsed ouptut format either "
-         "'AST' or 'HTML' "
-         "defaults to 'HTML'\n");
+  printf("%*s %s\n", -gap, "-h, --help ", "Show this help message");
+  printf("%*s %s\n", -gap, "-s, --source=<path>",
+         "string, file_path, dir_path to parse");
+  printf("%*s %s\n", -gap, "-d, --dir_depth=<depth>",
+         "depth of dir to search upto when source is "
+         "dir_path defaults to 1");
+  printf("%*s %s\n", -gap, "-o, --output_format=<format>",
+         "sparsed ouptut format either 'AST' or "
+         "'HTML' defaults to 'HTML'");
 }
